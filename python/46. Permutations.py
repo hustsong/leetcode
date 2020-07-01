24783948a201f846385e6977b5cebc08114d4ed7
gaosong <gaosong@oak-inc.com>
2020-07-01 17:54:45 +0800

    update

---
+++ python/46. Permutations.py
@@ -0,0 +1,18 @@
+from typing import List
+
+
+class Solution:
+  def permute(self, nums: List[int]) -> List[List[int]]:
+    
+
+  def _order_template(self, n: int) -> List[List[int]]:
+    if n == 1:
+      return [0]
+
+
+
+
+
+nums = [1,2,3]
+sol = Solution()
+print(sol.permute(nums))
