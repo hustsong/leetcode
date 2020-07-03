4588e7c41d9e5f71397860cc12ba965f48635bcf
root <root@DESKTOP-song.localdomain>
2020-07-03 21:47:05 +0800

    update

---
+++ python/221. Maximal Square.py
@@ -0,0 +1,23 @@
+from typing import List
+
+
+class Solution:
+    def maximalSquare(self, matrix: List[List[str]]) -> int:
+        side = 0
+        m, n = len(matrix), len(matrix[0])
+
+        for i in range(n):
+            row_sums = [0] * m
+            for j in range(i, n):
+                ones = 0
+                continuous = 0
+                for k in range(m):
+
+
+                if :
+                    break
+
+        return side * side
+
+
+sol = Solution()
--- python/46. Permutations.py
+++ python/46. Permutations.py
@@ -3,11 +3,36 @@ from typing import List
 
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        pass
+        n = len(nums)
+        if n <= 1:
+            return [nums]
+        if n == 2:
+            return [[nums[0], nums[1]], [nums[1], nums[0]]]
 
-    def _order_template(self, n: int) -> List[List[int]]:
+        orders = self._order_template(nums[1:])
+        orders = [[nums[0]] + order for order in orders]
+
+        ret = []
+        for order in orders:
+            for i in range(n):
+                curr = order[i:] + order[:i]
+                ret.append(curr)
+
+        return ret
+
+    def _order_template(self, nums: List[int]) -> List[List[int]]:
+        n = len(nums)
         if n == 1:
-            return [0]
+            return [[nums[0]]]
+
+        ret = []
+        for i in range(n):
+            base = [nums[i]]
+            orders = self._order_template([num for num in nums if num != nums[i]])
+            for order in orders:
+                ret.append(base + order)
+
+        return ret
 
 
 nums = [1, 2, 3]
