766ce56d9cce4f3eaebeec02c93b0174311d851e
root <root@DESKTOP-song.localdomain>
2020-07-05 23:32:42 +0800

    update

--- python/295. Find Median from Data Stream.py
+++ python/295. Find Median from Data Stream.py
@@ -52,51 +52,58 @@ class MedianFinder:
             self.median_node = self.nodes[num]
             self.median_node_pos = 0
             self.median = num
-        else:   # move median_node and median_pos
-            if num < self.median:
-                if int(self.total % 2) == 1:
+            return
+
+        # move median_node and median_pos
+        if num < self.median_node.val:   # move left
+            if int(self.total % 2) == 1:
+                self.median = self.median_node.val
+                if num == self.median_node.val:
+                    self.median_node_pos += 1
+            elif int(self.total % 2) == 0:
+                y = self.median
+                if self.median_node_pos > 0:
+                    x = y
+                    self.median_node_pos -= 1
+                else:
+                    prev_node = self.median_node.prev
+                    x = prev_node.val
+                    self.median_node_pos = prev_node.count - 1
+                    self.median_node = prev_node
+                self.median = float((x + y) / 2)
+        else:   # move right
+            if int(self.total % 2) == 1:
+                if self.median_node_pos < self.median_node.count - 1:
                     self.median = self.median_node.val
-                elif int(self.total % 2) == 0:
-                    y = self.median
-                    if self.median_node_pos > 0:
-                        x = y
-                        self.median_node_pos -= 1
-                    else:
-                        prev_node = self.median_node.prev
-                        x = prev_node.val
-                        self.median_node_pos = prev_node.count - 1
-                        self.median_node = prev_node
-                    self.median = float((x + y) / 2)
-            else:
-                if int(self.total % 2) == 1:
-                    if self.median_node_pos < self.median_node.count - 1:
-                        self.median = self.median_node.val
-                        self.median_node_pos += 1
-                    else:
-                        next_node = self.median_node.next
-                        self.median = next_node.val
-                        self.median_node = next_node
-                        self.median_node_pos = 0
-                elif int(self.total % 2) == 0:
-                    if self.median_node_pos < self.median_node.count - 1:
-                        self.median = self.median_node.val
-                    else:
-                        next_node = self.median_node.next
-                        self.median = float((self.median_node.val + next_node.val) / 2)
+                    self.median_node_pos += 1
+                else:
+                    next_node = self.median_node.next
+                    self.median = next_node.val
+                    self.median_node = next_node
+                    self.median_node_pos = 0
+            elif int(self.total % 2) == 0:
+                if self.median_node_pos < self.median_node.count - 1:
+                    self.median = self.median_node.val
+                else:
+                    next_node = self.median_node.next
+                    self.median = float((self.median_node.val + next_node.val) / 2)
 
     def findMedian(self) -> float:
-        print(self.median_node.val, self.median_node_pos, self.total)
         return self.median
+
+    def repr(self):
+        node = self.head
+        while node:
+            print(node.val, node.count)
+            node = node.next
 
 
 # Your MedianFinder object will be instantiated and called as such:
 obj = MedianFinder()
-obj.addNum(1)
+obj.addNum(40)
 print(obj.findMedian())
-obj.addNum(2)
+obj.addNum(12)
 print(obj.findMedian())
-obj.addNum(1)
+obj.addNum(16)
 print(obj.findMedian())
-obj.addNum(2)
-print(obj.findMedian())
-
+# obj.repr()
